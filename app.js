ff0bab69354a12c5cedfb3987278ad660a6c993b
dchristinej <75353290+dchristinej@users.noreply.github.com>
2021-10-04 14:34:00 +0800

    Add files via upload
    Turning it into own personal website

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const mobile_item = document.querySelector('.header .nav-bar .nav-list ul li a');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header .container');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
-})
+});
 
 document.addEventListener('scroll', () => {
     var scroll_position = window.scrollY;
@@ -15,11 +15,11 @@ document.addEventListener('scroll', () => {
     } else {
         header.style.backgroundColor = "transparent";
     }
-})
+});
 
-menu_item.forEach(item => {
+menu_item.forEach((item) => {
     item.addEventListener('click', () => {
         hamburger.classList.toggle('active');
         mobile_menu.classList.toggle('active');
-    })
-})
+    });
+});
